3843a078c0c3cb5f89f8ca6a057c20d32d52a24b
AvinashKadam10 <avinashanilkadam10@gmail.com>
2025-04-12 20:57:38 +0530

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import TopNavbar from "./TopNavbar";
-import logo from "/assets/1mdm-168x58.png";
+import logo from "../assets/1mdm-168x58.png";
 
 function Navbar() {
   return (
